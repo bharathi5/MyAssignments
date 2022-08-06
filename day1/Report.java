b8210387960e6beb50569f5327e3c4d69691a83a
bharathi5 <gurudivya5@gmail.com>
2022-08-06 21:29:16 +0530

    Add files via upload
    day1

---
+++ day1/Report.java
@@ -0,0 +1,19 @@
+package week1.day1;
+
+public class Report {
+
+	public static void main(String[] args) {
+		
+		Student student=new Student();
+		 
+		System.out.println(student.studentName);
+		System.out.println(student.rollNo);
+		System.out.println(student.collegeName);
+		System.out.println(student.markScored);
+		System.out.println(student.cgpa);
+		
+		
+
+	}
+
+}
---
+++ day1/Student.java
@@ -0,0 +1,16 @@
+package week1.day1;
+
+public class Student {
+     
+	String studentName="Divya";
+	int rollNo=24;
+	String collegeName="SASTRA UNIVERSITY";
+	short markScored=92;
+	float cgpa=8.5f;
+	
+	public static void main(String[] args) {
+		
+
+	}
+
+}
